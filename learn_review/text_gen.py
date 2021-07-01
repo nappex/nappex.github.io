b15dd309e4224e83454b0bdd399c0241b59e75a2
David Stroch <d.stroch@gmail.com>
2021-07-01 21:56:32 +0200

    First shot of running generator

--- learn_review/text_gen.py
+++ learn_review/text_gen.py
@@ -1,8 +1,24 @@
+from pathlib import Path
+
 from jinja2 import Environment, FileSystemLoader
+from yaml import safe_load
 
 
 TEMPLATES_DIR = 'templates'
-TEMPLATE_FILENAME ='readme_template.md'
+TEMPLATE_FILENAME ='index.html'
+INPUT_FILE = Path('data.yaml')
+OUTPUT_FILE = Path(__file__).parents[1] / 'output' / 'learn_review' / 'index.html'
 
 env = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
 template = env.get_template(TEMPLATE_FILENAME)
+
+data = safe_load(INPUT_FILE.read_text())
+
+print(data)
+
+
+completed = filter(lambda task: task.get('complete', False), data)
+
+rendered_template = template.render(completed=completed)
+
+OUTPUT_FILE.write_text(rendered_template)
