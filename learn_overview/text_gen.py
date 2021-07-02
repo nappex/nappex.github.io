9a6595cc1d1e9f112cf7ff793040895bad3874c7
David Stroch <d.stroch@gmail.com>
2021-07-02 13:49:58 +0200

    Add uncompleted tast, rename to overview

---
+++ learn_overview/text_gen.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader
+from yaml import safe_load
+
+
+TEMPLATE_FILE = Path('templates', 'index.html')
+INPUT_FILE = Path('data.yaml')
+OUTPUT_FILE = Path(__file__).parents[1] / 'output' / 'learn_review' / 'index.html'
+
+env = Environment(loader=FileSystemLoader(TEMPLATE_FILE.parent))
+template = env.get_template(TEMPLATE_FILE.name)
+
+data = safe_load(INPUT_FILE.read_text())
+
+completed = []
+uncompleted = []
+
+for task in data:
+    if task.get('completed', False):
+        completed.append(task)
+    else:
+        uncompleted.append(task)
+
+
+rendered_template = template.render(completed=completed, uncompleted=uncompleted)
+
+OUTPUT_FILE.write_text(rendered_template)
--- learn_review/data.py
+++
@@ -1,7 +0,0 @@
-DATASET = [
-    {
-        'title': 'PyLadies Ostrava',
-        'name': 'Python for beginners',
-        'start_date': '02/2019',
-    }
-]
