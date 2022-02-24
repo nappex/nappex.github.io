4813ff6d22668266692b0849d5d298d511220bf6
d.stroch <d.stroch@gmail.com>
2022-02-24 15:40:43 +0100

    Refactor code and add more exits

--- generate_content.sh
+++ generate_content.sh
@@ -20,26 +20,17 @@ python -m pip install --upgrade pip || exit 1
 if [ ! -d $PWD/pelican-plugins ]
 then
     pelican_plugins=(
-        "search"
-        "series"
-        "share-post"
-        "render-math"
-        "related-posts"
-        "neighbors"
+        "pelican-search"
+        "pelican-series"
+        "pelican-share-post"
+        "pelican-render-math"
+        "pelican-related-posts"
+        "pelican-neighbors"
     )
-    #for plugin in "${pelican_plugins[@]}"
-    #do
-    #    mkdir -p pelican-plugins/$plugin
-    #    src="https://github.com/pelican-plugins/${plugin}"
-    #    dst="pelican-plugins/${plugin}"
-    #    git clone --recursive $src $dst 1>/dev/null
-    #    echo "[INFO] pelican-plugin ${plugin} was created"
-    #    sleep 0.5
-    #done
+
     for plugin in "${pelican_plugins[@]}"
     do
-        pkg="pelican-${plugin}"
-        python -m pip install $pkg 1>/dev/null || exit 1
+        python -m pip install $plugin 1>/dev/null || exit 1
         echo "[INFO] pelican-plugin ${plugin} was created"
         sleep 1
     done
@@ -49,8 +40,9 @@ if [ ! $(pelican-themes -l | grep elegant) ]
 then
     mkdir -p pelican-themes/elegant
     src="https://github.com/Pelican-Elegant/elegant/"
-    git clone --recursive https://github.com/Pelican-Elegant/elegant pelican-themes/elegant
-    pelican-themes -i pelican-themes/elegant
+    dst="pelican-themes/elegant"
+    git clone --recursive $src $dst 1>/dev/null || exit 1
+    pelican-themes -i pelican-themes/elegant || exit 1
     echo "[INFO] pelican-themes created"
 fi
 
