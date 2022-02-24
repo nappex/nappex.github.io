5a75725e157eee0a245568a365e9310297fe4a94
d.stroch <d.stroch@gmail.com>
2022-02-24 13:35:01 +0100

    Add installation of pelican plugins

--- generate_content.sh
+++ generate_content.sh
@@ -7,7 +7,7 @@ then
     exit 1
 fi
 
-if ! [ -d $PWD/output ]
+if [ ! -d $PWD/output ]
 then
     echo "[INFO] Create output dir"
     git clone -b gh-pages git@github.com:nappex/nappex.github.io.git output
@@ -16,16 +16,37 @@ fi
 ./learn_overview/make_html.py
 source __venv__/bin/activate
 
-if ! [ -d $PWD/pelican-plugins ]
+if [ ! -d $PWD/pelican-plugins ]
 then
-    mkdir pelican-plugins
-    git clone --recursive https://github.com/getpelican/pelican-plugins pelican-plugins/
-    echo "[INFO] pelican-plugins created"
+    plugins=(
+        "extract_toc"
+        "neighbors"
+        "related_posts"
+        "render_math"
+        "series"
+        "share_post"
+    )
+
+    for plugin in "${plugins[@]}"
+    do
+        mkdir -p pelican-plugins/$plugin
+        src="https://github.com/getpelican/pelican-plugins/${plugin}"
+        dst="pelican-plugins/${plugin}"
+        git clone --recursive $src $dst 1>/dev/null
+        echo "[INFO] pelican-plugin ${plugin} was created"
+        sleep 0.5
+    done
+
+    mkdir -p pelican-plugins/search
+    git clone --recursive https://github.com/pelican-plugins/search pelican-plugins/search 1>/dev/null
+    echo "[INFO] pelican-plugin search was created"
+    sleep 0.5
 fi
 
-if ! [ $(pelican-themes -l | grep elegant) ]
+if [ ! $(pelican-themes -l | grep elegant) ]
 then
     mkdir -p pelican-themes/elegant
+    src="https://github.com/Pelican-Elegant/elegant/"
     git clone --recursive https://github.com/Pelican-Elegant/elegant pelican-themes/elegant
     pelican-themes -i pelican-themes/elegant
     echo "[INFO] pelican-themes created"
